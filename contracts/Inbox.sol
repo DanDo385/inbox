e572e7431d1b3e8ad5182baaebccfd1fc2350ce8
Daniel Magro <dando385@yahoo.com>
2024-02-25 20:56:20 -0500

    -m

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,4 +1,18 @@
-pragma solidity ^0.4.17;
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Inbox {
+    string public message;
+
+    function inbox(string memory initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
 // linter warnings (red underline) about pragma version can igonored!
 
 // contract code will go here
